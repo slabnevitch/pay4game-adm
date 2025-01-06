498e14b9c31c9b5e22c334ac4cc15ebcf396b004
Slabnevitch <handofschpilrain@mail.ru>
2025-01-06 17:45:51 +0500

    settings page ready

--- js/common.js
+++ js/common.js
@@ -99,6 +99,36 @@
 				}
 			}
 		}
+
+		// Управление высотой многострочных <textarea>
+		if(document.querySelectorAll('.form__area').length > 0 ){
+			autoHeightOnResize();
+			Array.prototype.slice.call(document.querySelectorAll('.form__area')).forEach(function(elem) {
+				elem.addEventListener('input', autoHeightOnInput);
+			});
+
+			window.addEventListener('resize', function(e) {
+				Array.prototype.slice.call(document.querySelectorAll('.form__area')).forEach(function(elem) {
+					autoHeightOnResize();
+				});
+			});
+			
+			function autoHeightOnInput(e) {
+			  // Сбрасываем высоту, чтобы она соответствовала текущему объему текста
+			  e.target.style.height = '1px';
+			  e.target.style.height =  e.target.scrollHeight + 'px';
+			}
+
+			function autoHeightOnResize() {
+			  // Сбрасываем высоту, чтобы она соответствовала текущему объему текста
+				Array.prototype.slice.call(document.querySelectorAll('.form__area')).forEach(function(elem) {
+					elem.style.height = '1px';
+					elem.style.height =  elem.scrollHeight + 'px';
+				});
+			}
+
+		}
+		// END Управление высотой многострочных <textarea>
 		
 	});
 })();
